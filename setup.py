75c6eec8f11dbaf704a47e3d54d17058b557e366
demohari <kishore_koneru@yahoo.com>
2023-01-10 17:35:37 +0000

    update

--- setup.py
+++ setup.py
@@ -1,22 +1,10 @@
-from setuptools import find_packages,setup
-from typing import List
+from setuptools import find_packages, setup
 
-def get_requirements()->List[str]:
-    """
-    This function will return list of requirements
-    """
-    requirement_list:List[str] = []
-
-    """
-    Write a code to read requirements.txt file and append each requirements in requirement_list variable.
-    """
-    return requirement_list
 
 setup(
     name="sensor",
     version="0.0.1",
     author="demohari",
-    author_email="kishore_koneru@gmail.com",
-    packages = find_packages(),
-    install_requires=get_requirements(),#["pymongo==4.2.0"],
+    author_email="kishore_koneru@yahoo.com",
+    packages=find_packages(),
 )
